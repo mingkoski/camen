c5e2d310924486e71a1378e4d4365fc2b39cf524
mingkoski <mingkoski@gmail.com>
2023-08-30 23:48:21 +0900

    build

--- build/object/world.d.ts
+++ build/object/world.d.ts
@@ -1,14 +1,13 @@
 /// <reference types="dist" />
 import { CamenObject } from "./object.js";
 export declare class World {
-    private _device;
     protected _vertices: Float32Array;
-    get vertices(): Float32Array;
     protected _vertexBuffer: GPUBuffer;
-    get vertexBuffer(): GPUBuffer;
     protected _vertexBufferLayouts: GPUVertexBufferLayout[];
-    get vertexBufferLayouts(): GPUVertexBufferLayout[];
     protected _objects: Record<string, CamenObject>;
+    get vertices(): Float32Array;
+    get vertexBuffer(): GPUBuffer;
+    get vertexBufferLayouts(): GPUVertexBufferLayout[];
     constructor();
     add(object: CamenObject): void;
 }
